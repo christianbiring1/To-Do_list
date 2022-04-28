e95d6f5cbe540badf5eced5acc5d57230504a358
christianbiring1 <christianbiringanine22@gmail.com>
2022-04-28 10:24:25 +0200

    setting the webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
 import './style.css';
-const div = document.createElement('div');
-div.innerHTML = 'hello webpack';
-document.body.appendChild(div);
+
+const container = document.querySelector('.container');
+
+const tasks = [
+  {
+    description: 'Finish up the project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Start new project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Joggling',
+    completed: false,
+    index: 3,
+  },
+];
+
+const populate = () => {
+  tasks.forEach((task) => {
+    const wrapper = document.createElement('ul');
+    wrapper.classList.add('wrapper');
+
+    const li = document.createElement('li');
+    li.classList.add('list-item');
+
+    const box = document.createElement('input');
+    box.setAttribute('type', 'checkbox');
+
+    const title = document.createElement('span');
+    title.classList.add('title');
+    title.innerHTML = task.description;
+    const option = document.createElement('span');
+    option.classList.add('option');
+
+    wrapper.appendChild(li);
+    li.append(box, title, option);
+    container.append(wrapper);
+  });
+};
+populate();
+console.log('hello world');
